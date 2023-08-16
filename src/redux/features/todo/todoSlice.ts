a579db0e5526460a1b35af393dbb5f37cf265109
Anish Sharma <sharmaanish023@gmail.com>
2023-08-17 01:56:36 +0545

    Add global type for todo

--- src/redux/features/todo/todoSlice.ts
+++ src/redux/features/todo/todoSlice.ts
@@ -1,15 +1,21 @@
+import { TTodo } from "@/types";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface IState {
+  todos: TTodo[];
+}
+
+const initialState: IState = {
+  todos: [],
+};
 
 // create a redux slice
 export const todoSlice = createSlice({
   name: "extraction",
-  initialState: {
-    loading: false,
-    todos: {},
-  },
+  initialState,
   reducers: {
-    addTodo(state, action) {
-      state.todos = action.payload;
+    addTodo(state, action: PayloadAction<TTodo>) {
+      state.todos.push(action.payload);
     },
   },
   extraReducers: () => {},
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type TTodo = {
+  todo: string;
+  isCompleted: boolean;
+  id: string;
+};
